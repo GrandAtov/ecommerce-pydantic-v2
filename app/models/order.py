15571433565d23ef66e2880873baa7de2924fbb9
Grand Atov <highquglowup@gmail.com>
2026-07-14 03:07:21 +0700

    add order.py

---
+++ app/models/order.py
@@ -0,0 +1,41 @@
+from uuid import UUID
+from typing import Annotated, Literal
+
+from pydantic import Field, model_validator
+
+from .base import BaseSchema
+from .customer import Customer
+from .product import Product
+
+class OrderItem(BaseSchema):
+    id: UUID
+    product: Product
+    quantity: Annotated[int, Field(gt=0)]
+    price: Annotated[float, Field(gt=0)]
+    
+class Order(BaseSchema):
+    id: UUID
+    customer: Customer
+    items: list[OrderItem]
+    status: Literal[
+        "pending",
+        "paid",
+        "shipped",
+        "completed",
+        "cancelled"
+    ]
+    
+    @model_validator(mode="after")
+    def order_validator(self):
+        if not self.items:
+            raise ValueError("Order harus memiliki minimal satu item")
+        
+        total = sum(
+            item.quantity * item.price
+            for item in self.items
+        )
+        
+        if total <= 0:
+            raise ValueError("Total harga semua item tidak valid")
+        return self
+    
